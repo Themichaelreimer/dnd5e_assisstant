495d6991263f8394cb23c8ed4bf1b17320ba64c1
Ryzan Malabuyoc <malabuyoc.ryzan@gmail.com>
2018-03-18 12:26:22 -0500

    Updated S3 region

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,10 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-addEventListener("direct-upload:error", event => {
-  event.preventDefault()
-  const { id, error } = event.detail
-  console.log(error);
-  // const element = document.getElementById(`direct-upload-${id}`)
-  // element.classList.add("direct-upload--error")
-  // element.setAttribute("title", error)
-});
