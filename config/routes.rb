f25477ab701239b903b0455689b3efffc7714f48
Michael Reimer <themichaelreimer@gmail.com>
2018-05-24 12:08:02 -0500

    Generated user view; updated router

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :products
+  resources :users, except: :destroy
 
   root to: 'products#index'
 
-  # devise_scope :user do
+  #devise_scope :user do
   #   delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
-  # end
+  #end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
