1212d67523e87feedfad6a925473b286aadad056
Michael Reimer <themichaelreimer@gmail.com>
2018-05-25 12:01:43 -0500

    Login/logout system works with email signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
+	protect_from_forgery with: :exception
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+	protected
 
-  def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_up) << :name
-  	devise_parameter_sanitizer.for(:account_name) << :name
-  end
-  
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :products
-  resources :users, except: :destroy
+  resources :users
 
   root to: 'products#index'
 
