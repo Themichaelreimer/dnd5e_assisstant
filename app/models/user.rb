26f01ed6990fea4b8be6cea06e9f05f7d1732a43
Michael Reimer <themichaelreimer@gmail.com>
2018-06-05 22:58:21 -0500

    Implemented math parser with dice rolls

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  #validates :name, :presence => true, :length => {:in 1..30}
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   #resources :templates
   #resources :actors
+  root to: 'sessions#new'
   #root to: 'templates#index'
+
+  get '/signup' => 'users#new'
+  get '/logout' => 'sessions#destroy'
+  get '/login' => 'sessions#new'
+  post '/users' => 'users#create'
 
 end
